b02b9c1830b71961ff36691faf8fea8fcc532ea0
Juan Carlos Moreno <jcrogel@gmail.com>
2015-07-20 14:04:01 -0700

    Initial Commit

---
+++ DebugWifiTests/DebugWifiTests.swift
@@ -0,0 +1,36 @@
+//
+//  DebugWifiTests.swift
+//  DebugWifiTests
+//
+//  Created by Juan Carlos Moreno on 7/20/15.
+//  Copyright (c) 2015 Juan C Moreno. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DebugWifiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
